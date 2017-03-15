0808628a886c6f06c3f97004bdb852a43027d0d8
ocadaruma <ocadaruma@gmail.com>
2017-03-15 20:34:47 +0900

    implement BqPrepareStatement (wip)

---
+++ src/main/scala/scalikejdbc/bigquery/BqParameter.scala
@@ -0,0 +1,28 @@
+package scalikejdbc.bigquery
+
+abstract class BqParameter
+
+object BqParameter {
+
+  case class Int64(value: Long) extends BqParameter
+
+  case class Float64(value: Double) extends BqParameter
+
+  case class Bool(value: Boolean) extends BqParameter
+
+  case class String(value: java.lang.String) extends BqParameter
+
+  case class Bytes(value: scala.Array[Byte]) extends BqParameter
+
+  case class Date(value: java.time.LocalDate) extends BqParameter
+
+  case class DateTime(value: java.time.LocalDateTime) extends BqParameter
+
+  case class Time(value: java.time.LocalTime) extends BqParameter
+
+  case class Timestamp(value: java.time.ZonedDateTime) extends BqParameter
+
+  // TODO: case class Array[A <: BqParameter](value: scala.Array[A]) extends BqParameter
+
+  // TODO: case class Struct() extends BqParameter
+}
--- src/main/scala/scalikejdbc/bigquery/BqPreparedStatement.scala
+++ src/main/scala/scalikejdbc/bigquery/BqPreparedStatement.scala
@@ -6,35 +6,52 @@ import java.net.URL
 import java.sql.{Array => SqlArray, _}
 import java.util.Calendar
 
+import scala.collection.concurrent.TrieMap
+import scala.collection.mutable
+
 class BqPreparedStatement extends PreparedStatement {
 
-  def setByte(parameterIndex: Int, x: Byte): Unit = ???
+  private[this] val parameterMap: mutable.Map[Int, BqParameter] = TrieMap.empty
+
+  def setByte(parameterIndex: Int, x: Byte): Unit =
+    parameterMap(parameterIndex) = BqParameter.Int64(x)
 
   def getParameterMetaData: ParameterMetaData = ???
 
-  def setRef(parameterIndex: Int, x: Ref): Unit = ???
+  def setRef(parameterIndex: Int, x: Ref): Unit =
+    throw new UnsupportedOperationException("setRef is not supported")
 
   def clearParameters(): Unit = ???
 
-  def setBytes(parameterIndex: Int, x: Array[Byte]): Unit = ???
+  def setBytes(parameterIndex: Int, x: Array[Byte]): Unit =
+    parameterMap(parameterIndex) = BqParameter.Bytes(x)
 
-  def setBinaryStream(parameterIndex: Int, x: InputStream, length: Int): Unit = ???
+  def setBinaryStream(parameterIndex: Int, x: InputStream, length: Int): Unit =
+    throw new UnsupportedOperationException("setBinaryStream is not supported")
 
-  def setBinaryStream(parameterIndex: Int, x: InputStream, length: Long): Unit = ???
+  def setBinaryStream(parameterIndex: Int, x: InputStream, length: Long): Unit =
+    throw new UnsupportedOperationException("setBinaryStream is not supported")
 
-  def setBinaryStream(parameterIndex: Int, x: InputStream): Unit = ???
+  def setBinaryStream(parameterIndex: Int, x: InputStream): Unit =
+    throw new UnsupportedOperationException("setBinaryStream is not supported")
 
-  def setAsciiStream(parameterIndex: Int, x: InputStream, length: Int): Unit = ???
+  def setAsciiStream(parameterIndex: Int, x: InputStream, length: Int): Unit =
+    throw new UnsupportedOperationException("setAsciiStream is not supported")
 
-  def setAsciiStream(parameterIndex: Int, x: InputStream, length: Long): Unit = ???
+  def setAsciiStream(parameterIndex: Int, x: InputStream, length: Long): Unit =
+    throw new UnsupportedOperationException("setAsciiStream is not supported")
 
-  def setAsciiStream(parameterIndex: Int, x: InputStream): Unit = ???
+  def setAsciiStream(parameterIndex: Int, x: InputStream): Unit =
+    throw new UnsupportedOperationException("setAsciiStream is not supported")
 
-  def setObject(parameterIndex: Int, x: scala.Any, targetSqlType: Int): Unit = ???
+  def setObject(parameterIndex: Int, x: scala.Any, targetSqlType: Int): Unit =
+    throw new UnsupportedOperationException("setObject is not supported")
 
-  def setObject(parameterIndex: Int, x: scala.Any): Unit = ???
+  def setObject(parameterIndex: Int, x: scala.Any): Unit =
+    throw new UnsupportedOperationException("setObject is not supported")
 
-  def setObject(parameterIndex: Int, x: scala.Any, targetSqlType: Int, scaleOrLength: Int): Unit = ???
+  def setObject(parameterIndex: Int, x: scala.Any, targetSqlType: Int, scaleOrLength: Int): Unit =
+    throw new UnsupportedOperationException("setObject is not supported")
 
   def setDate(parameterIndex: Int, x: Date): Unit = ???
 
@@ -44,165 +61,240 @@ class BqPreparedStatement extends PreparedStatement {
 
   def setTimestamp(parameterIndex: Int, x: Timestamp, cal: Calendar): Unit = ???
 
-  def setUnicodeStream(parameterIndex: Int, x: InputStream, length: Int): Unit = ???
+  def setUnicodeStream(parameterIndex: Int, x: InputStream, length: Int): Unit =
+    throw new UnsupportedOperationException("setUnicodeStream is not supported")
 
   def getMetaData: ResultSetMetaData = ???
 
-  def setBlob(parameterIndex: Int, x: Blob): Unit = ???
+  def setBlob(parameterIndex: Int, x: Blob): Unit =
+    throw new UnsupportedOperationException("setBlob is not supported")
 
-  def setBlob(parameterIndex: Int, inputStream: InputStream, length: Long): Unit = ???
+  def setBlob(parameterIndex: Int, inputStream: InputStream, length: Long): Unit =
+    throw new UnsupportedOperationException("setBlob is not supported")
 
-  def setBlob(parameterIndex: Int, inputStream: InputStream): Unit = ???
+  def setBlob(parameterIndex: Int, inputStream: InputStream): Unit =
+    throw new UnsupportedOperationException("setBlob is not supported")
 
-  def addBatch(): Unit = ???
+  def addBatch(): Unit =
+    throw new UnsupportedOperationException("addBatch is not supported")
 
-  def execute(): Boolean = ???
+  def execute(): Boolean =
+    throw new UnsupportedOperationException("execute is not supported")
 
-  def executeQuery(): ResultSet = ???
+  def executeQuery(): ResultSet =
+    throw new UnsupportedOperationException("executeQuery is not supported")
 
-  def setNClob(parameterIndex: Int, value: NClob): Unit = ???
+  def setNClob(parameterIndex: Int, value: NClob): Unit =
+    throw new UnsupportedOperationException("setNClob is not supported")
 
-  def setNClob(parameterIndex: Int, reader: Reader, length: Long): Unit = ???
+  def setNClob(parameterIndex: Int, reader: Reader, length: Long): Unit =
+    throw new UnsupportedOperationException("setNClob is not supported")
 
-  def setNClob(parameterIndex: Int, reader: Reader): Unit = ???
+  def setNClob(parameterIndex: Int, reader: Reader): Unit =
+    throw new UnsupportedOperationException("setNClob is not supported")
 
-  def setArray(parameterIndex: Int, x: SqlArray): Unit = ???
+  def setArray(parameterIndex: Int, x: SqlArray): Unit =
+    throw new UnsupportedOperationException("setArray is not supported")
 
-  def setNCharacterStream(parameterIndex: Int, value: Reader, length: Long): Unit = ???
+  def setNCharacterStream(parameterIndex: Int, value: Reader, length: Long): Unit =
+    throw new UnsupportedOperationException("setNCharacterStream is not supported")
 
-  def setNCharacterStream(parameterIndex: Int, value: Reader): Unit = ???
+  def setNCharacterStream(parameterIndex: Int, value: Reader): Unit =
+    throw new UnsupportedOperationException("setNCharacterStream is not supported")
 
-  def setURL(parameterIndex: Int, x: URL): Unit = ???
+  def setURL(parameterIndex: Int, x: URL): Unit =
+    throw new UnsupportedOperationException("setURL is not supported")
 
-  def setRowId(parameterIndex: Int, x: RowId): Unit = ???
+  def setRowId(parameterIndex: Int, x: RowId): Unit =
+    throw new UnsupportedOperationException("setRowId is not supported")
 
-  def setSQLXML(parameterIndex: Int, xmlObject: SQLXML): Unit = ???
+  def setSQLXML(parameterIndex: Int, xmlObject: SQLXML): Unit =
+    throw new UnsupportedOperationException("setSQLXML is not supported")
 
-  def setString(parameterIndex: Int, x: String): Unit = ???
+  def setString(parameterIndex: Int, x: String): Unit =
+    parameterMap(parameterIndex) = BqParameter.String(x)
 
-  def setFloat(parameterIndex: Int, x: Float): Unit = ???
+  def setFloat(parameterIndex: Int, x: Float): Unit =
+    parameterMap(parameterIndex) = BqParameter.Float64(x)
 
-  def setNString(parameterIndex: Int, value: String): Unit = ???
+  def setNString(parameterIndex: Int, value: String): Unit =
+    throw new UnsupportedOperationException("setNString is not supported")
 
-  def setBoolean(parameterIndex: Int, x: Boolean): Unit = ???
+  def setBoolean(parameterIndex: Int, x: Boolean): Unit =
+    parameterMap(parameterIndex) = BqParameter.Bool(x)
 
-  def setDouble(parameterIndex: Int, x: Double): Unit = ???
+  def setDouble(parameterIndex: Int, x: Double): Unit =
+    parameterMap(parameterIndex) = BqParameter.Float64(x)
 
-  def setBigDecimal(parameterIndex: Int, x: BigDecimal): Unit = ???
+  def setBigDecimal(parameterIndex: Int, x: BigDecimal): Unit =
+    parameterMap(parameterIndex) = BqParameter.String(x.toPlainString)
 
-  def executeUpdate(): Int = ???
+  def executeUpdate(): Int =
+    throw new UnsupportedOperationException("executeUpdate is not supported")
 
   def setTime(parameterIndex: Int, x: Time): Unit = ???
 
   def setTime(parameterIndex: Int, x: Time, cal: Calendar): Unit = ???
 
-  def setShort(parameterIndex: Int, x: Short): Unit = ???
+  def setShort(parameterIndex: Int, x: Short): Unit =
+    parameterMap(parameterIndex) = BqParameter.Int64(x)
 
-  def setLong(parameterIndex: Int, x: Long): Unit = ???
+  def setLong(parameterIndex: Int, x: Long): Unit =
+    parameterMap(parameterIndex) = BqParameter.Int64(x)
 
-  def setCharacterStream(parameterIndex: Int, reader: Reader, length: Int): Unit = ???
+  def setCharacterStream(parameterIndex: Int, reader: Reader, length: Int): Unit =
+    throw new UnsupportedOperationException("setCharacterStream is not supported")
 
-  def setCharacterStream(parameterIndex: Int, reader: Reader, length: Long): Unit = ???
+  def setCharacterStream(parameterIndex: Int, reader: Reader, length: Long): Unit =
+    throw new UnsupportedOperationException("setCharacterStream is not supported")
 
-  def setCharacterStream(parameterIndex: Int, reader: Reader): Unit = ???
+  def setCharacterStream(parameterIndex: Int, reader: Reader): Unit =
+    throw new UnsupportedOperationException("setCharacterStream is not supported")
 
-  def setClob(parameterIndex: Int, x: Clob): Unit = ???
+  def setClob(parameterIndex: Int, x: Clob): Unit =
+    throw new UnsupportedOperationException("setClob is not supported")
 
-  def setClob(parameterIndex: Int, reader: Reader, length: Long): Unit = ???
+  def setClob(parameterIndex: Int, reader: Reader, length: Long): Unit =
+    throw new UnsupportedOperationException("setClob is not supported")
 
-  def setClob(parameterIndex: Int, reader: Reader): Unit = ???
+  def setClob(parameterIndex: Int, reader: Reader): Unit =
+    throw new UnsupportedOperationException("setClob is not supported")
 
   def setNull(parameterIndex: Int, sqlType: Int): Unit = ???
 
   def setNull(parameterIndex: Int, sqlType: Int, typeName: String): Unit = ???
 
-  def setInt(parameterIndex: Int, x: Int): Unit = ???
+  def setInt(parameterIndex: Int, x: Int): Unit =
+    parameterMap(parameterIndex) = BqParameter.Int64(x)
 
-  def setMaxFieldSize(max: Int): Unit = ???
+  def setMaxFieldSize(max: Int): Unit =
+    throw new UnsupportedOperationException("setMaxFieldSize is not supported")
 
-  def getMoreResults: Boolean = ???
+  def getMoreResults: Boolean =
+    throw new UnsupportedOperationException("getMoreResults is not supported")
 
-  def getMoreResults(current: Int): Boolean = ???
+  def getMoreResults(current: Int): Boolean =
+    throw new UnsupportedOperationException("getMoreResults is not supported")
 
   def clearWarnings(): Unit = ???
 
-  def getGeneratedKeys: ResultSet = ???
+  def getGeneratedKeys: ResultSet =
+    throw new UnsupportedOperationException("getGeneratedKeys is not supported")
 
-  def closeOnCompletion(): Unit = ???
+  def closeOnCompletion(): Unit =
+    throw new UnsupportedOperationException("closeOnCompletion is not supported")
 
-  def cancel(): Unit = ???
+  def cancel(): Unit =
+    throw new UnsupportedOperationException("cancel is not supported")
 
-  def getResultSet: ResultSet = ???
+  def getResultSet: ResultSet =
+    throw new UnsupportedOperationException("getResultSet is not supported")
 
-  def setPoolable(poolable: Boolean): Unit = ???
+  def setPoolable(poolable: Boolean): Unit =
+    throw new UnsupportedOperationException("setPoolable is not supported")
 
-  def isPoolable: Boolean = ???
+  def isPoolable: Boolean =
+    throw new UnsupportedOperationException("isPoolable is not supported")
 
-  def setCursorName(name: String): Unit = ???
+  def setCursorName(name: String): Unit =
+    throw new UnsupportedOperationException("setCursorName is not supported")
 
-  def getUpdateCount: Int = ???
+  def getUpdateCount: Int =
+    throw new UnsupportedOperationException("getUpdateCount is not supported")
 
-  def addBatch(sql: String): Unit = ???
+  def addBatch(sql: String): Unit =
+    throw new UnsupportedOperationException("addBatch is not supported")
 
-  def getMaxRows: Int = ???
+  def getMaxRows: Int =
+    throw new UnsupportedOperationException("getMaxRows is not supported")
 
-  def execute(sql: String): Boolean = ???
+  def execute(sql: String): Boolean =
+    throw new UnsupportedOperationException("execute is not supported")
 
-  def execute(sql: String, autoGeneratedKeys: Int): Boolean = ???
+  def execute(sql: String, autoGeneratedKeys: Int): Boolean =
+    throw new UnsupportedOperationException("execute is not supported")
 
-  def execute(sql: String, columnIndexes: Array[Int]): Boolean = ???
+  def execute(sql: String, columnIndexes: Array[Int]): Boolean =
+    throw new UnsupportedOperationException("execute is not supported")
 
-  def execute(sql: String, columnNames: Array[String]): Boolean = ???
+  def execute(sql: String, columnNames: Array[String]): Boolean =
+    throw new UnsupportedOperationException("execute is not supported")
 
-  def executeQuery(sql: String): ResultSet = ???
+  def executeQuery(sql: String): ResultSet =
+    throw new UnsupportedOperationException("executeQuery is not supported")
 
-  def getResultSetType: Int = ???
+  def getResultSetType: Int =
+    throw new UnsupportedOperationException("getResultSetType is not supported")
 
-  def setMaxRows(max: Int): Unit = ???
+  def setMaxRows(max: Int): Unit =
+    throw new UnsupportedOperationException("setMaxRows is not supported")
 
-  def getFetchSize: Int = ???
+  def getFetchSize: Int =
+    throw new UnsupportedOperationException("getFetchSize is not supported")
 
-  def getResultSetHoldability: Int = ???
+  def getResultSetHoldability: Int =
+    throw new UnsupportedOperationException("getResultSetHoldability is not supported")
 
-  def setFetchDirection(direction: Int): Unit = ???
+  def setFetchDirection(direction: Int): Unit =
+    throw new UnsupportedOperationException("setFetchDirection is not supported")
 
-  def getFetchDirection: Int = ???
+  def getFetchDirection: Int =
+    throw new UnsupportedOperationException("getFetchDirection is not supported")
 
-  def getResultSetConcurrency: Int = ???
+  def getResultSetConcurrency: Int =
+    throw new UnsupportedOperationException("getResultSetConcurrency is not supported")
 
-  def clearBatch(): Unit = ???
+  def clearBatch(): Unit =
+    throw new UnsupportedOperationException("clearBatch is not supported")
 
-  def close(): Unit = ???
+  def close(): Unit =
+    throw new UnsupportedOperationException("close is not supported")
 
-  def isClosed: Boolean = ???
+  def isClosed: Boolean =
+    throw new UnsupportedOperationException("isClosed is not supported")
 
-  def executeUpdate(sql: String): Int = ???
+  def executeUpdate(sql: String): Int =
+    throw new UnsupportedOperationException("executeUpdate is not supported")
 
-  def executeUpdate(sql: String, autoGeneratedKeys: Int): Int = ???
+  def executeUpdate(sql: String, autoGeneratedKeys: Int): Int =
+    throw new UnsupportedOperationException("executeUpdate is not supported")
 
-  def executeUpdate(sql: String, columnIndexes: Array[Int]): Int = ???
+  def executeUpdate(sql: String, columnIndexes: Array[Int]): Int =
+    throw new UnsupportedOperationException("executeUpdate is not supported")
 
-  def executeUpdate(sql: String, columnNames: Array[String]): Int = ???
+  def executeUpdate(sql: String, columnNames: Array[String]): Int =
+    throw new UnsupportedOperationException("executeUpdate is not supported")
 
-  def getQueryTimeout: Int = ???
+  def getQueryTimeout: Int =
+    throw new UnsupportedOperationException("getQueryTimeout is not supported")
 
-  def getWarnings: SQLWarning = ???
+  def getWarnings: SQLWarning =
+    throw new UnsupportedOperationException("getWarnings is not supported")
 
-  def setFetchSize(rows: Int): Unit = ???
+  def setFetchSize(rows: Int): Unit =
+    throw new UnsupportedOperationException("setFetchSize is not supported")
 
-  def setQueryTimeout(seconds: Int): Unit = ???
+  def setQueryTimeout(seconds: Int): Unit =
+    throw new UnsupportedOperationException("setQueryTimeout is not supported")
 
-  def executeBatch(): Array[Int] = ???
+  def executeBatch(): Array[Int] =
+    throw new UnsupportedOperationException("executeBatch is not supported")
 
-  def setEscapeProcessing(enable: Boolean): Unit = ???
+  def setEscapeProcessing(enable: Boolean): Unit =
+    throw new UnsupportedOperationException("setEscapeProcessing is not supported")
 
-  def getConnection: Connection = ???
+  def getConnection: Connection =
+    throw new UnsupportedOperationException("getConnection is not supported")
 
-  def getMaxFieldSize: Int = ???
+  def getMaxFieldSize: Int =
+    throw new UnsupportedOperationException("getMaxFieldSize is not supported")
 
-  def isCloseOnCompletion: Boolean = ???
+  def isCloseOnCompletion: Boolean =
+    throw new UnsupportedOperationException("isCloseOnCompletion is not supported")
 
-  def unwrap[T](iface: Class[T]): T = ???
+  def unwrap[T](iface: Class[T]): T =
+    throw new UnsupportedOperationException("unwrap is not supported")
 
-  def isWrapperFor(iface: Class[_]): Boolean = ???
+  def isWrapperFor(iface: Class[_]): Boolean =
+    throw new UnsupportedOperationException("isWrapperFor is not supported")
 }
