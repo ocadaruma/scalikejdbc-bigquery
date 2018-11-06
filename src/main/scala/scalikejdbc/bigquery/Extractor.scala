a997e66315c781196c26aafd4ed06f64f239f3c8
ta.tanaka <takayahilton@gmail.com>
2018-11-06 16:01:28 +0900

    Add api first and iterator to Extractor

--- src/main/scala/scalikejdbc/bigquery/Extractor.scala
+++ src/main/scala/scalikejdbc/bigquery/Extractor.scala
@@ -4,9 +4,20 @@ import scalikejdbc._
 
 class Extractor[A](statement: SQLSyntax, f: WrappedResultSet => A) {
 
+  def iterator: Runner[Iterator[A]] = Runner(statement)(_.map(f))
+
   def list: Runner[Seq[A]] = Runner(statement)(_.map(f).toList)
 
-  def single: Runner[Option[A]] = Runner(statement)(_.map(f).toStream.headOption)
+  def single: Runner[Option[A]] = Runner(statement) { rs =>
+    val rows = (rs map f).toList
+    rows match {
+      case Nil => None
+      case one :: Nil => Option(one)
+      case _ => throw TooManyRowsException(1, rows.size)
+    }
+  }
+
+  def first: Runner[Option[A]] = Runner(statement)(_.map(f).toStream.headOption)
 }
 
 trait ExtractorBuilder {
